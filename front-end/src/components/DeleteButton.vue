4260043565c63d58a84ccd401cc55edd3320beea
TimboNZ76 <custompc2010@live.com>
2021-08-26 12:31:31 +1200

    Added Delete Button Code

--- front-end/src/components/DeleteButton.vue
+++ front-end/src/components/DeleteButton.vue
@@ -1,10 +1,21 @@
 <template>
-  
+    <button @onClick="deletePost" class="delete-button">Delete Post</button>
 </template>
 
 <script>
 export default {
-
+    methods:{
+        async delete(url) {
+            const response = await fetch ("http://localhost:3000/posts", {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            });
+            const resData = '"Post Deleted...';
+            return resData;
+        }
+    }
 }
 </script>
 
